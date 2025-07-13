1b0bb7ab76a59ae842621d1f7983f52451c8fc6d
Nikita Kulagin <melifaro.nbk@gmail.com>
2025-07-14 01:15:56 +0300

    homework3 : implementation of COWBuffer (#3)

---
+++ homework3/homework3.go
@@ -0,0 +1,48 @@
+package homework3
+
+import "unsafe"
+
+type COWBuffer struct {
+	data []byte
+	refs *int
+}
+
+func NewCOWBuffer(data []byte) COWBuffer {
+	var refs int = 1
+	return COWBuffer{
+		data: data,
+		refs: &refs,
+	}
+}
+
+func (b *COWBuffer) Clone() COWBuffer {
+	*b.refs++
+	return COWBuffer{
+		data: b.data,
+		refs: b.refs,
+	}
+}
+
+func (b *COWBuffer) Close() {
+	if b.refs != nil && *b.refs > 0 {
+		*b.refs--
+	}
+}
+
+func (b *COWBuffer) Update(index int, value byte) bool {
+	if index < 0 || index >= len(b.data) {
+		return false
+	}
+	if *b.refs > 1 {
+		*b.refs--
+		newBuf := NewCOWBuffer(make([]byte, len(b.data)))
+		copy(newBuf.data, b.data)
+		*b = newBuf
+	}
+	b.data[index] = value
+	return true
+}
+
+func (b *COWBuffer) String() string {
+	return unsafe.String(unsafe.SliceData(b.data), len(b.data))
+}
